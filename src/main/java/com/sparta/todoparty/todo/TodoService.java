1be7caaf99d43c67c850fbb8050f24b959094aec
JEONGYOUNGGYU <zkdlf90@gmail.com>
2023-11-20 20:37:33 +0900

    feat(todo) : 할일카드 단건조회 기능 API 적용

--- src/main/java/com/sparta/todoparty/todo/TodoService.java
+++ src/main/java/com/sparta/todoparty/todo/TodoService.java
@@ -4,12 +4,13 @@ import com.sparta.todoparty.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class TodoService {
 
     private final TodoRepository todoRepository;
-
 
     public TodoResponseDto createPost(TodoRequestDto todoRequestDto, User user){
         Todo todo = new Todo(todoRequestDto);
@@ -18,5 +19,12 @@ public class TodoService {
         todoRepository.save(todo);
 
         return new TodoResponseDto(todo);
+    }
+
+    public TodoResponseDto getTodo(Long todoId) {
+        Todo todo = todoRepository.findById(todoId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 할일 ID 입니다."));
+        return new TodoResponseDto(todo);
+
     }
 }
